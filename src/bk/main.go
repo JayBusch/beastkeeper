07dfe9bd5785fbe73c20eba68d874a80b0fabf9d
John V. Busch <jbusch@dataverve.com>
2016-02-17 04:24:35 -0500

    some basic config file parsing working

--- src/bk/main.go
+++ src/bk/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
-	//	"encoding/json"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/twinj/uuid"
 	"gopkg.in/alecthomas/kingpin.v2"
+	"io/ioutil"
+	"log"
 	"net"
-	"os"
+	"strings"
 )
 
 // This var block contains the command line commands and flags for BeastKeeper.
@@ -18,26 +21,56 @@ var (
 
 // This var block contains globally declared variables for BeastKeeper.
 var (
-	beastKeeperMasterConfiguration *BeastKeeperConfiguration
+	beastKeeperMasterConfiguration BeastKeeperConfiguration
 )
 
 //Type and Enum construct for describing Instance types
 type InstanceType int
 
 const (
-	VM  InstanceType = iota
-	POD InstanceType = iota
+	VM InstanceType = iota
+	BM InstanceType = iota
 )
 
-// Instance structs contain the data required to describe an individual FreeBSD instance deployed anywhere
-// This can be bare-metal, or virtual machine either local or at a provider.
-// Application containers such as jetpack pods are not included in this, and have their own data structure
+func (self InstanceType) UnmarshalJSON(b []byte) error {
+
+	switch strings.Trim(string(b), "\"") {
+	case "VM":
+		self = VM
+		return nil
+	case "BM":
+		self = BM
+		return nil
+	default:
+		return errors.New("Un-Recognized InstanceType")
+	}
+}
+
+type UUID struct {
+	UUID uuid.UUID
+}
+
+func (self *UUID) UnmarshalJSON(b []byte) error {
+
+	s := strings.Trim(string(b), "\"")
+	uuid, uuidErr := uuid.Parse(s)
+	self.UUID = uuid
+	if self.UUID == nil || uuidErr != nil {
+		return errors.New("Could not parse UUID")
+	}
+	return nil
+}
+
+// Instance structs contain the data required to describe an individual FreeBSD
+// instance deployed anywhere. This can be bare-metal, or virtual machine either
+// local or at a provider.  Application containers such as jetpack pods are not
+// included in this, and have their own data structure
 type Instance struct {
-	ID         uuid.UUID
+	ID         *UUID `json:",UUID"`
 	Label      string
 	Type       InstanceType
-	address    net.IP
-	containers []ApplicationContainerInstance
+	Address    net.IP
+	Containers []ApplicationContainerInstance
 }
 
 //Type and Enum construct for describing ApplicationContainer types
@@ -48,40 +81,42 @@ const (
 	Docker  ApplicationContainerType = iota
 )
 
-// ApplicationContainer structs contain the data requied to describe isntances of OS level virtualized
-// application containers such as jetpack pods.
+// ApplicationContainer structs contain the data requied to describe isntances
+// of OS level virtualized application containers such as jetpack pods.
 type ApplicationContainerInstance struct {
 	ID      uuid.UUID
 	Label   string
 	Type    ApplicationContainerType
-	address net.IP
+	Address net.IP
 }
 
-// BeastKeeperConfiguration structs describe a single full configuration of the BeastKeeper application
-// There will initially only be one instance of this type held in a global variable; but others may
-// be introduced later if we add features to manage multiple configurations
+// BeastKeeperConfiguration structs describe a single full configuration of the
+// BeastKeeper application. There will initially only be one instance of this
+// type held in a global variable; but others may be introduced later if we add
+// features to manage multiple configurations
 type BeastKeeperConfiguration struct {
-	instances []InstanceType
+	Instances []Instance
 }
 
-// parseConfigFile reads a config file and marshalls the JSON data to a GO struct representing
-// the configuration of BeastKeeper.
-// The User can speficy the config file name on the command line with the -c or --configFileName flags.
-// If no file is specified, the defailt value of "config.bk" is used.
+// parseConfigFile reads a config file and marshalls the JSON data to a GO
+// struct representing the configuration of BeastKeeper. The User can specify
+// the config file name on the command line with the -c or --configFileName
+// flags. If no file is specified, the defailt value of "config.bk" is used.
 // BeastKeeper looks in the current directory for that default file.
-func parseConfigFile(configFileName string) *BeastKeeperConfiguration {
+func parseConfigFile(configFileName string) BeastKeeperConfiguration {
 
-	configFile, err := os.Open(configFileName)
-	_ = configFile
-	if err != nil {
-		fmt.Printf("%v\n", "Config File Not Found")
-	} else {
-		fmt.Printf("%v\n", "Config File Found")
-	}
+	configFileData, fileErr := ioutil.ReadFile(configFileName)
 
 	config := new(BeastKeeperConfiguration)
-	return config
+	if fileErr != nil {
+		log.Fatal(fileErr)
+	}
+	jsonErr := json.Unmarshal(configFileData, &config)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
 
+	return *config
 }
 
 func command_config_print() {
--- src/bk/main_test.go
+++ src/bk/main_test.go
@@ -1,11 +1,37 @@
 package main
 
 import (
+	"io/ioutil"
 	"testing"
 )
 
 func TestParseConfigFile(t *testing.T) {
-	test := parseConfigFile("testconfig.bk")
-	_ = test
-	t.Fatalf("Initial Failing Test")
+	// Test config as JSON string, we are going to write this to the
+	// file system and allow the parseConfigFile function to read it back.
+	// The idea being that unit tests should be self contained and not rely
+	// on external resources.
+	testConfigAsString := `
+	{
+		"instances": [{
+			"ID" : "2237b51a-39aa-4720-b68a-d1ee214e9272",
+			"Label" : "test_instance_1",
+			"Type" : "VM",
+			"Address" : "192.242.2.200"
+			}]	
+	}
+	`
+
+	testConfigFileName := "../../test/config/unitTest_config_file.bk"
+
+	ioutil.WriteFile(testConfigFileName, []byte(testConfigAsString), 0644)
+
+	testConfig := parseConfigFile(testConfigFileName)
+
+	// Check the values on our returned config file to see if they match
+	if len(testConfig.Instances) < 1 {
+		t.Fatalf("No data was unmarshalled")
+	} else if testConfig.Instances[0].Label != "test_instance_1" {
+		t.Fatalf("Label does not match")
+	}
+
 }
